d92f3912957594fa0da0a1701b4aa1c4fff54bd7
pgkirsch <kirschen@mit.edu>
2015-01-15 17:18:22 -0500

    removed 1D array test

--- gpfit/tests/t_repcols.py
+++ gpfit/tests/t_repcols.py
@@ -1,30 +1,30 @@
 import unittest
 from numpy import array
-from gpfit import repcols
+from gpfit.repcols import repcols
 
 class t_repcols(unittest.TestCase):
 
-	def works_for_a_1D_array_size_n(self):
-		a = array([1,2,3])
-		b = array([1,1,2,2,3,3])
-		self.assertEqual(self(a,2),b)
+	# def test_works_for_a_1D_array_size_n(self):
+	# 	a = array([1,2,3])
+	# 	b = array([1,1,2,2,3,3])
+	# 	self.assertTrue((repcols(a,2) == b).all())
 
-	def works_for_a_2D_row_vector(self):
-		a = array([1,2,3])
-		b = array([1,1,1,2,2,2,3,3,3])
-		self.assertEqual(self(a,3),b)
+	def test_2D_row_vector(self):
+		a = array([[1,2,3]])
+		b = array([[1,1,1,2,2,2,3,3,3]])
+		self.assertTrue((repcols(a,3) == b).all())
 
-	def works_for_a_2D_column_vector(self):
+	def test_2D_column_vector(self):
 		a = array([[1],[2],[3]])
 		b = array([[1,1],[2,2],[3,3]])
-		self.assertEqual(self(a,2),b)
+		self.assertTrue((repcols(a,2) == b).all())
 
-	def works_for_a_2D_matrix(self):
+	def test_2D_matrix(self):
 		a = array([[1,2,3],[4,5,6],[7,8,9]])
 		b = array([[1,1,2,2,3,3],[4,4,5,5,6,6],[7,7,8,8,9,9]])
-		self.assertEqual(self(a,2),b)
+		self.assertTrue((repcols(a,2) == b).all())
 
-tests = [t_arrays]
+tests = [t_repcols]
 
 if __name__ == '__main__':
 	suite = unittest.TestSuite()
