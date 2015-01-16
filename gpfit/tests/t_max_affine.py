0f627f5bb3f3f1943f9f933e6a9e7f38ee18ac80
pgkirsch <kirschen@mit.edu>
2015-01-16 14:03:02 -0500

    added max_affine unit test

---
+++ gpfit/tests/t_max_affine.py
@@ -0,0 +1,33 @@
+import unittest
+from gpfit.max_affine import max_affine
+from numpy import arange, newaxis
+
+class t_max_affine(unittest.TestCase):
+
+	x = arange(0.,16.)[:,newaxis]
+	ba = arange(1.,5.).reshape(2,2)
+
+	y, dydba = max_affine(x,ba) 
+
+	def test_y_size(self):
+		self.assertEqual(self.y.size, self.x.size)
+
+	def test_y_ndim(self):
+		self.assertEqual(self.y.ndim, 1)
+
+	def test_dydba_shape(self):
+		self.assertEqual(self.dydba.shape, (self.x.size, self.ba.size))
+
+	def test_dydba_ndim(self):
+		self.assertEqual(self.dydba.ndim, 2)
+
+tests = [t_max_affine]
+
+if __name__ == '__main__':
+	suite = unittest.TestSuite()
+	loader = unittest.TestLoader()
+
+	for t in tests:
+		suite.addTests(loader.loadTestsFromTestCase(t))
+
+	unittest.TextTestRunner(verbosity=2).run(suite)
