af9aaa432b7367a153bb09796144da7923152d7d
Warren Hoburg <whoburg@mit.edu>
2015-04-03 15:08:05 -0400

    pep8

--- gpfit/lse_implicit.py
+++ gpfit/lse_implicit.py
@@ -1,6 +1,7 @@
 from numpy import zeros, ones, spacing, exp, log, tile
 
-def lse_implicit(x,alpha):        
+
+def lse_implicit(x, alpha):
     '''
     Implicit Log-sum-exponential function with derivatives
     - sums across the second dimension of x
@@ -21,7 +22,7 @@ def lse_implicit(x,alpha):
             y:      dependent variable fit. Returns one y for every row of x.
                     [n-element 1D array], n is number of data points
 
-            dydx:   dydx gives the deriv of each y wrt each x 
+            dydx:   dydx gives the deriv of each y wrt each x
                     [(n x d) 2D array]
 
             dydalpha:
@@ -30,55 +31,57 @@ def lse_implicit(x,alpha):
     '''
     bverbose = False
 
-    tol = 10*spacing(1) 
-    npt,nx = x.shape
+    tol = 10*spacing(1)
+    npt, nx = x.shape
 
     if not alpha.size == nx:
         raise Exception('alpha size mismatch')
 
-    alphamat = tile(alpha, (npt,1))
+    alphamat = tile(alpha, (npt, 1))
 
-    m = x.max(1)  #maximal x values
+    m = x.max(1)  # maximal x values
+    # distance from m; note h <= 0 for all entries
+    h = x - (tile(m, (nx, 1))).T
+    # (unless x has infs; in this case h has nans;
+    # can prob deal w/ this gracefully)
+    # should also deal with alpha==inf case gracefully
 
-    h = x - (tile(m, (nx, 1))).T   #distance from m; note h <= 0 for all entries
-    #(unless x has infs; in this case h has nans; can prob deal w/ this gracefully)
-    #should also deal with alpha==inf case gracefully
+    L = zeros((npt,))  # initial guess. note y = m + L
 
-    L = zeros((npt,))  #initial guess. note y = m + L
+    Lmat = (tile(L, (nx, 1))).T
 
-    Lmat = (tile(L,(nx,1))).T
-
-    #initial eval
+    # initial eval
     expo = exp(alphamat*(h-Lmat))
     alphaexpo = alphamat*expo
-    sumexpo = expo.sum(axis=1) #<<<<<<<<<<<check that lack of transpose isnt issue<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    sumexpo = expo.sum(axis=1)  # todo: check that lack of transpose isnt issue
     sumalphaexpo = alphaexpo.sum(axis=1)
     f = log(sumexpo)
     dfdL = -alphaexpo.sum(axis=1)/sumexpo
     neval = 1
-    i = abs(f) > tol #inds to update
+    i = abs(f) > tol  # inds to update
 
     while any(i):
-        L[i] = L[i] - f[i]/dfdL[i]    #newton step
-        #re-evaluate
-        Lmat[i,:] = (tile(L[i],(nx,1))).T
-        expo[i,:] = exp(alphamat[i,:] * (h[i,:] - Lmat[i,:]))
-        alphaexpo[i,:] = alphamat[i,:] * expo[i,:]
-        sumexpo[i] = expo[i,:].sum(axis=1)
-        sumalphaexpo[i,] = alphaexpo[i,:].sum(axis=1) # sumalphaexpo[i,:] <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        L[i] = L[i] - f[i]/dfdL[i]    # newton step
+        # re-evaluate
+        Lmat[i, :] = (tile(L[i], (nx, 1))).T
+        expo[i, :] = exp(alphamat[i, :] * (h[i, :] - Lmat[i, :]))
+        alphaexpo[i, :] = alphamat[i, :] * expo[i, :]
+        sumexpo[i] = expo[i, :].sum(axis=1)
+        sumalphaexpo[i, ] = alphaexpo[i, :].sum(axis=1)  # sumalphaexpo[i,:]
         f[i] = log(sumexpo[i])
-        dfdL[i] = -sumalphaexpo[i,]/sumexpo[i]
+        dfdL[i] = -sumalphaexpo[i, ]/sumexpo[i]
         neval = neval + 1
 
-        #update inds that need to be evaluated
+        # update inds that need to be evaluated
         i[i] = abs(f[i]) > tol
 
     if bverbose:
-        print('lse_implicit converged in ' + repr(neval) + ' newton-raphson steps')
+        print('lse_implicit converged in ' +
+              repr(neval) + ' newton-raphson steps')
 
     y = m + L
 
-    dydx = alphaexpo/(tile(sumalphaexpo, (nx,1))).T
-    dydalpha = (h - Lmat)*expo/(tile(sumalphaexpo, (nx,1))).T
-    
+    dydx = alphaexpo/(tile(sumalphaexpo, (nx, 1))).T
+    dydalpha = (h - Lmat)*expo/(tile(sumalphaexpo, (nx, 1))).T
+
     return y, dydx, dydalpha
--- gpfit/lse_scaled.py
+++ gpfit/lse_scaled.py
@@ -1,6 +1,7 @@
 from numpy import array, tile, exp, log
 
-def lse_scaled(x,alpha):
+
+def lse_scaled(x, alpha):
     '''
     Log-sum-exponential function with derivatives
     - sums across the second dimension of x
@@ -14,10 +15,10 @@ def lse_scaled(x,alpha):
                     Double
 
     OUTPUTS:
-            y:         dependent variable fit. Returns one y for every row of x.
+            y: dependent variable fit. Returns one y for every row of x.
                     [n-element 1D array], n is number of data points
 
-            dydx:    dydx gives the deriv of each y wrt each x 
+            dydx: dydx gives the deriv of each y wrt each x
                     [n x ? 2D array]
 
             dydalpha:
@@ -27,11 +28,12 @@ def lse_scaled(x,alpha):
 
     _, n = x.shape
 
-    m = x.max(axis=1)  #maximal x values
+    m = x.max(axis=1)  # maximal x values
 
-    h = x - (tile(m, (n, 1))).T # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #distance from m; note h <= 0 for all entries
-    #(unless x has infs; in this case h has nans; can prob deal w/ this gracefully)
-    #should also deal with alpha==inf case gracefully
+    h = x - (tile(m, (n, 1))).T  # distance from m; note h <= 0 for all entries
+    # (unless x has infs; in this case h has nans;
+    # can prob deal w/ this gracefully)
+    # should also deal with alpha==inf case gracefully
 
     expo = exp(alpha*h)
     sumexpo = expo.sum(axis=1)
@@ -40,8 +42,8 @@ def lse_scaled(x,alpha):
 
     y = L + m
 
-    dydx = expo/(tile(sumexpo, (n, 1))).T #<<<<<<<<<<<<<<<<<<<<
-    #note that sum(dydx,2)==1, i.e. dydx is a probability distribution
+    dydx = expo/(tile(sumexpo, (n, 1))).T
+    # note that sum(dydx,2)==1, i.e. dydx is a probability distribution
     dydalpha = ((h*expo).sum(axis=1)/sumexpo - L)/alpha
-    
+
     return y, dydx, dydalpha
