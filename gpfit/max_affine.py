83cac5fddc634b78646627f1f164db080b3da7fe
Warren Hoburg <whoburg@mit.edu>
2015-04-18 01:19:56 -0400

    minor cleanups

--- gpfit/max_affine.py
+++ gpfit/max_affine.py
@@ -1,41 +1,44 @@
-from numpy import zeros, hstack, reshape, ones, dot, arange, equal, ix_
+import numpy as np
 
 
-def max_affine(x, ba, flag=False):
-    '''
+def max_affine(x, ba):
+    """
     Evaluates max affine function at values of x, given a set of
-    max affine fit parameters. 
+    max affine fit parameters.
 
-    INPUTS:
-            x:      Independent variable data
-                        2D array [nPoints x nDim]
+    INPUTS
+    ------
+        x: 2D array [nPoints x nDim]
+            Independent variable data
 
-            ba:     max affine fit parameters
-                        2D array [b1, a11,..a1k,.... bk,ak1,..akk]
+        ba: 2D array
+            max affine fit parameters
+            [[b1, a11, ... a1k]
+             [ ....,          ]
+             [bk, ak1, ... akk]]
 
-    OUTPUTS:
-            y:      MA approximation to log transformed data
-                        1D array [nPoints]
-
-            dydba:  dydba
-                        2D array [nPoints x (nDim + 1)*K]
-
-    '''
+    OUTPUTS
+    -------
+        y: 1D array [nPoints]
+            Max affine output
+        dydba: 2D array [nPoints x (nDim + 1)*K]
+            dydba
+    """
     npt, dimx = x.shape
-    K = ba.size/(dimx+1)
-    ba = reshape(ba, (dimx+1, K), order='F')  # 'F' gives Fortran indexing
+    K = ba.size/(dimx + 1)
+    ba = np.reshape(ba, (dimx + 1, K), order='F')  # 'F' gives Fortran indexing
 
     # augment data with column of ones
-    X = hstack((ones((npt, 1)), x))
+    X = np.hstack((np.ones((npt, 1)), x))
 
-    y, partition = (dot(X, ba)).max(1), (dot(X, ba)).argmax(1)
+    y, partition = np.dot(X, ba).max(1), np.dot(X, ba).argmax(1)
 
     # The not-sparse sparse version
-    dydba = zeros((npt, (dimx + 1)*K))
+    dydba = np.zeros((npt, (dimx + 1)*K))
     for k in range(K):
-        inds = equal(partition, k)
-        indadd = (dimx+1)*k
-        ixgrid = ix_(inds.nonzero()[0], indadd + arange(dimx+1))
+        inds = np.equal(partition, k)
+        indadd = (dimx + 1)*k
+        ixgrid = np.ix_(inds.nonzero()[0], indadd + np.arange(dimx+1))
         dydba[ixgrid] = X[inds, :]
 
     return y, dydba
