8890143626c1f3061dd8c80393dea17028394e17
mjburton <mjburton@mit.edu>
2016-01-19 21:15:59 -0500

    class changed to run run_tests file

--- gpfit/tests/t_ex6_1.py
+++ gpfit/tests/t_ex6_1.py
@@ -19,35 +19,35 @@ class t_ex6_1_ISMA(unittest.TestCase):
 
 
 class t_ex6_1_SMA(unittest.TestCase):
-    '''
-    SMA unit tests based on example 6.1 from GPfit paper
-    '''
-    m = 501
-    u = logspace(0,log10(3),501)
-    w = (u**2 + 3)/(u+1)**2
-    x = log(u)
-    y = log(w)
-    K = 3
-
-    cstrt, rms_error = fit(x, y, K, "SMA")
-
     def test_rms_error(self):
+        '''
+        SMA unit tests based on example 6.1 from GPfit paper
+        '''
+        m = 501
+        u = logspace(0,log10(3),501)
+        w = (u**2 + 3)/(u+1)**2
+        x = log(u)
+        y = log(w)
+        K = 3
+
+        cstrt, rms_error = fit(x, y, K, "SMA")
+
         self.assertTrue(self.rms_error < 1e-4)
 
 class t_ex6_1_MA(unittest.TestCase):
-    '''
-    MA unit tests based on example 6.1 from GPfit paper
-    '''
-    m = 501
-    u = logspace(0,log10(3),501)
-    w = (u**2 + 3)/(u+1)**2
-    x = log(u)
-    y = log(w)
-    K = 3
-
-    cstrt, rms_error = fit(x, y, K, "MA")
-
     def test_rms_error(self):
+        '''
+        MA unit tests based on example 6.1 from GPfit paper
+        '''
+        m = 501
+        u = logspace(0,log10(3),501)
+        w = (u**2 + 3)/(u+1)**2
+        x = log(u)
+        y = log(w)
+        K = 3
+
+        cstrt, rms_error = fit(x, y, K, "MA")
+
         self.assertTrue(self.rms_error < 1e-2)
 
 
--- gpfit/tests/t_ex6_3.py
+++ gpfit/tests/t_ex6_3.py
@@ -4,54 +4,54 @@ from gpfit.fit import fit
 from numpy.random import random_sample
 
 class t_ex6_3_ISMA(unittest.TestCase):
-    '''
-    ISMA unit tests based on example 6.3 from GPfit paper
-    '''
-    Vdd = random_sample(1000,) + 1
-    Vth = 0.2*random_sample(1000,) + 0.2
-    P = Vdd**2 + 30*Vdd*exp(-(Vth-0.06*Vdd)/0.039)
-    u = vstack((Vdd,Vth))
-    x = log(u)
-    y = log(P)
-    K = 4
-
-    cstrt, rms_error = fit(x, y, K, "ISMA")
-
     def test_rms_error(self):
+        '''
+        ISMA unit tests based on example 6.3 from GPfit paper
+        '''
+        Vdd = random_sample(1000,) + 1
+        Vth = 0.2*random_sample(1000,) + 0.2
+        P = Vdd**2 + 30*Vdd*exp(-(Vth-0.06*Vdd)/0.039)
+        u = vstack((Vdd,Vth))
+        x = log(u)
+        y = log(P)
+        K = 4
+
+        cstrt, rms_error = fit(x, y, K, "ISMA")
+
         self.assertTrue(self.rms_error < 5e-4)
 
 class t_ex6_3_SMA(unittest.TestCase):
-    '''
-    SMA unit tests based on example 6.3 from GPfit paper
-    '''
-    Vdd = random_sample(1000,) + 1
-    Vth = 0.2*random_sample(1000,) + 0.2
-    P = Vdd**2 + 30*Vdd*exp(-(Vth-0.06*Vdd)/0.039)
-    u = vstack((Vdd,Vth))
-    x = log(u)
-    y = log(P)
-    K = 4
-
-    cstrt, rms_error = fit(x, y, K, "SMA")
-
     def test_rms_error(self):
+        '''
+        SMA unit tests based on example 6.3 from GPfit paper
+        '''
+        Vdd = random_sample(1000,) + 1
+        Vth = 0.2*random_sample(1000,) + 0.2
+        P = Vdd**2 + 30*Vdd*exp(-(Vth-0.06*Vdd)/0.039)
+        u = vstack((Vdd,Vth))
+        x = log(u)
+        y = log(P)
+        K = 4
+
+        cstrt, rms_error = fit(x, y, K, "SMA")
+
         self.assertTrue(self.rms_error < 5e-4)
 
 class t_ex6_3_MA(unittest.TestCase):
-    '''
-    MA unit tests based on example 6.3 from GPfit paper
-    '''
-    Vdd = random_sample(1000,) + 1
-    Vth = 0.2*random_sample(1000,) + 0.2
-    P = Vdd**2 + 30*Vdd*exp(-(Vth-0.06*Vdd)/0.039)
-    u = vstack((Vdd,Vth))
-    x = log(u)
-    y = log(P)
-    K = 4
-
-    cstrt, rms_error = fit(x, y, K, "MA")
-
     def test_rms_error(self):
+        '''
+        MA unit tests based on example 6.3 from GPfit paper
+        '''
+        Vdd = random_sample(1000,) + 1
+        Vth = 0.2*random_sample(1000,) + 0.2
+        P = Vdd**2 + 30*Vdd*exp(-(Vth-0.06*Vdd)/0.039)
+        u = vstack((Vdd,Vth))
+        x = log(u)
+        y = log(P)
+        K = 4
+
+        cstrt, rms_error = fit(x, y, K, "MA")
+
         self.assertTrue(self.rms_error < 1e-2)
 
 
