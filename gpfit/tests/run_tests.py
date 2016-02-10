c6cb4577c1c81680788f41a799f4fddab9192366
Warren Hoburg <whoburg@mit.edu>
2016-02-09 21:26:34 -0500

    attempt to resolve relative imports

--- gpfit/tests/run_tests.py
+++ gpfit/tests/run_tests.py
@@ -1,40 +1,40 @@
 import unittest
 TESTS = []
 
-import t_LM
+from gpfit.tests import t_LM
 TESTS += t_LM.tests
 
-import t_generic_resid_fun
+from gpfit.tests import t_generic_resid_fun
 TESTS += t_generic_resid_fun.tests
 
-import t_lse_implicit
+from gpfit.tests import t_lse_implicit
 TESTS += t_lse_implicit.tests
 
-import t_lse_scaled
+from gpfit.tests import t_lse_scaled
 TESTS += t_lse_scaled.tests
 
-import t_max_affine_init
+from gpfit.tests import t_max_affine_init
 TESTS += t_max_affine_init.tests
 
-import t_max_affine
+from gpfit.tests import t_max_affine
 TESTS += t_max_affine.tests
 
-import t_softmax_affine
+from gpfit.tests import t_softmax_affine
 TESTS += t_softmax_affine.tests
 
-import t_implicit_softmax_affine
+from gpfit.tests import t_implicit_softmax_affine
 TESTS += t_implicit_softmax_affine.tests
 
-import t_print_fit
+from gpfit.tests import t_print_fit
 TESTS += t_print_fit.tests
 
-import t_ex6_1
+from gpfit.tests import t_ex6_1
 TESTS += t_ex6_1.tests
 
-import t_ex6_3
+from gpfit.tests import t_ex6_3
 TESTS += t_ex6_3.tests
 
-import t_examples
+from gpfit.tests import t_examples
 TESTS += t_examples.TESTS
 
 def run():
