f2f78130fec07fff6d878167a1fb83e696746a0b
pgkirsch <kirschen@mit.edu>
2016-08-17 21:37:12 -0700

    creates setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+LICENSE = """The MIT License (MIT)
+
+Copyright (c) 2015 MIT Convex Optimization Group
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
+setup(
+    name="gpfit",
+    description="Package for fitting geometric programming models to data",
+    author="MIT Department of Aeronautics and Astronautics",
+    author_email="gpkit@mit.edu",
+    url="https://github.com/hoburg/gpfit",
+    install_requires=["numpy", "scipy", "gpkit"],
+    version="0.0",
+    packages=["gpfit"],
+    license=LICENSE,
+)
