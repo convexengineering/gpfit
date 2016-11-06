ea1742e61314e21a10eabdabde45eb2815efc8d6
pgkirsch <kirschen@mit.edu>
2016-11-06 14:59:18 -0800

    Accidentally renamed uu, also pint

--- gpfit/plot_fit.py
+++ gpfit/plot_fit.py
@@ -11,7 +11,7 @@ def plot_fit_1d(u, w, K=1, fitclass='MA', plotspace='log'):
 
     params, _ = fit(x, y, K, fitclass)
 
-    xx = np.linspace(min(x), max(x), 1000)
+    uu = np.linspace(min(u), max(u), 1000)
     if fitclass == 'MA':
         A = params[[i for i in range(K*2) if i % 2 != 0]]
         B = params[[i for i in range(K*2) if i % 2 == 0]]
@@ -47,6 +47,7 @@ def plot_fit_1d(u, w, K=1, fitclass='MA', plotspace='log'):
     ax.legend(['Data'] + stringlist, 
                loc='best')
 
+    plt.show()
     return f, ax
 
 if __name__ == "__main__":
