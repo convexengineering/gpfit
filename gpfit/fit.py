8ca56d8e460cb810afee3302b8cc26f5bda0a7d8
Michael Burton <mjburton@mit.edu>
2017-05-23 09:38:20 -0400

    add method to return fit parameters as DataFrame (#68)
    * return error; initial try at returning dataframe
    
    * return data set with relevent parameters for fit storage
    
    * bounds for 1 dependent var
    
    * add docstring for dataframe method
    
    * import pandas local to method to pass tests
    
    * remove unused casting of alpha
    
    * pylint
    
    * change from exs to exps; cast alpha in get_dataframe method
    
    * add documentation on get dataframe
    
    * rm max error from return line
    
    * disable invalid name
    
    * ignore import error

--- gpfit/fit.py
+++ gpfit/fit.py
@@ -1,5 +1,5 @@
 "Implements the all-important 'fit' function."
-from numpy import ones, exp, sqrt, mean, square, hstack
+from numpy import ones, exp, sqrt, mean, square, hstack, array
 from gpkit import NamedVariables, VectorVariable, Variable, NomialArray
 from .implicit_softmax_affine import implicit_softmax_affine
 from .softmax_affine import softmax_affine
@@ -8,13 +8,13 @@ from .levenberg_marquardt import levenberg_marquardt
 from .ba_init import ba_init
 from .print_fit import print_ISMA, print_SMA, print_MA
 
-
 ALPHA_INIT = 10
 RFUN = {"ISMA": implicit_softmax_affine,
         "SMA": softmax_affine,
         "MA": max_affine}
 
 
+# pylint: disable=invalid-name
 def get_params(ftype, K, xdata, ydata):
     "Perform least-squares fitting optimization."
     def rfun(params):
@@ -36,6 +36,8 @@ def get_params(ftype, K, xdata, ydata):
 
 
 # pylint: disable=too-many-locals
+# pylint: disable=too-many-branches
+# pylint: disable=import-error
 def fit(xdata, ydata, K, ftype="ISMA"):
     """
     Fit a log-convex function to multivariate data, returning a GP constraint
@@ -57,7 +59,7 @@ def fit(xdata, ydata, K, ftype="ISMA"):
 
     # Check data is in correct form
     if ydata.ndim > 1:
-        raise ValueError('Dependent data should be in the form of a 1D numpy array')
+        raise ValueError('Dependent data should be a 1D numpy array')
 
     # Transform to column vector
     xdata = xdata.reshape(xdata.size, 1) if xdata.ndim == 1 else xdata.T
@@ -88,15 +90,23 @@ def fit(xdata, ydata, K, ftype="ISMA"):
     if ftype == "ISMA":
         # constraint of the form 1 >= c1*u1^exp1*u2^exp2*w^(-alpha) + ....
         lhs, rhs = 1, (monos/w**alpha).sum()
+        cs = rhs.cs
+        exps = hstack([[exs[e] for e in exs if "w" not in str(e)]
+                       for exs in list(rhs.exps)])
         print_ISMA(A, B, alpha, d, K)
     elif ftype == "SMA":
         # constraint of the form w^alpha >= c1*u1^exp1 + c2*u2^exp2 +....
         lhs, rhs = w**alpha, monos.sum()
+        cs = rhs.cs
+        exps = hstack([[exs[e] for e in exs] for exs in list(rhs.exps)])
         print_SMA(A, B, alpha, d, K)
     elif ftype == "MA":
         # constraint of the form w >= c1*u1^exp1, w >= c2*u2^exp2, ....
         lhs, rhs = w, monos
+        cs = hstack([fn.cs for fn in rhs])
+        exps = hstack([[fn.exps[0][e] for e in fn.exps[0]] for fn in rhs])
         print_MA(A, B, d, K)
+
 
     if K == 1:
         # when possible, return an equality constraint
@@ -122,5 +132,57 @@ def fit(xdata, ydata, K, ftype="ISMA"):
 
     cstrt.evaluate = evaluate
     rms_error = sqrt(mean(square(evaluate(xdata.T)-ydata)))
+    max_error = sqrt(max(square(evaluate(xdata.T)-ydata)))
+
+    def get_dataframe(xdata):
+        """
+        Returns fit parameters as a dataframe
+        -------------------------------------
+        INPUTS
+            xdata:      Independent variable data
+                            2D numpy array [nDim, nPoints]
+            ** Must have pandas installed
+
+        OUTPUT
+            df:         Fitted constraint parameters
+                            Pandas dataframe
+                            ex:
+                w**a1 = c1 * u_1**e11 * u_2**e12 + c2 * u_1**e21 * u_2**e22
+                df.columns = ["ftype", "K", "d", "c1", "c2", "e11", "e12",
+                              "e22", "e21", "e22", "a1", "lb1", "ub1", "lb2",
+                              "ub2", "rms_error", "max_error"]
+                lb = lower bound
+                ub = upper bound
+        """
+        import pandas as pd
+
+        bounds = []
+        if d == 1:
+            bounds.append(min(xdata))
+            bounds.append(max(xdata))
+        else:
+            for i in range(d):
+                bounds.append(min(xdata[i]))
+                bounds.append(max(xdata[i]))
+
+        if ftype != "ISMA":
+            alphas = [alpha]
+
+        data = hstack([[ftype, K, d], cs, exps, alphas, exp(bounds),
+                       [rms_error, max_error]])
+        df = pd.DataFrame(data).transpose()
+        colnames = array(["ftype", "K", "d"])
+        colnames = hstack([colnames, ["c%d" % k for k in range(1, K+1)]])
+        colnames = hstack([colnames, ["e%d%d" % (k, i) for k in range(1, K+1)
+                                      for i in range(1, d+1)]])
+        colnames = hstack([colnames, ["a%d" % i for i in
+                                      range(1, len(alphas)+1)]])
+        colnames = hstack([colnames, hstack([["lb%d" % i, "ub%d" % i]
+                                             for i in range(1, d+1)])])
+        colnames = hstack([colnames, ["rms_err", "max_err"]])
+        df.columns = colnames
+        return df
+
+    cstrt.get_dataframe = get_dataframe
 
     return cstrt, rms_error
