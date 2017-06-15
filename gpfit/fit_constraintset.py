236ce8c0d62706fc630f0375f2a6fec39679933a
mjburton <mjburton@mit.edu>
2017-06-14 22:14:18 -0700

    make xfoilfitCS

--- gpfit/fit_constraintset.py
+++ gpfit/fit_constraintset.py
@@ -25,10 +25,9 @@ class FitCS(ConstraintSet):
                             will call XFOIL in process solution to check fit
                                 str - ("xxx.dat", "naca xxxx")
     """
-    def __init__(self, fitdata, ivar=None, dvars=None, name="", err_margin=None,
-                 airfoil=False):
+    def __init__(self, fitdata, ivar=None, dvars=None, name="",
+                 err_margin=None):
 
-        self.airfoil = airfoil
         self.fitdata = fitdata
 
         if ivar is None:
@@ -111,50 +110,77 @@ class FitCS(ConstraintSet):
     def process_result(self, result):
         """
         make sure fit result is within bounds of fitted data
-        if data comes from Xfoil and airfoil is provided check against xfoil
         """
         super(FitCS, self).process_result(result)
 
         if amax([abs(result["sensitivities"]["constants"][self.mfac])]) < 1e-5:
             return None
 
-        if self.airfoil:
-            from .xfoilWrapper import xfoil_comparison
-            cl, re = 0.0, 0.0
-            for dvar in self.dvars:
-                if "Re" in str(dvar):
-                    re = result(dvar)
-                if "C_L" in str(dvar):
-                    cl = result(dvar)
-            cd = result(self.ivar)
-            if not hasattr(cl, "__len__") and hasattr(re, "__len__"):
-                cl = [cl]*len(re)
-            err, cdx = xfoil_comparison(self.airfoil, cl, re, cd)
-            ind = where(err > 0.05)[0]
-            for i in ind:
-                msg = ("Drag error for %s is %.2f. Re=%.1f; CL=%.4f;"
-                       " Xfoil cd=%.6f, GP sol cd=%.6f" %
-                       (", ".join(self.ivar.descr["models"]), err[i], re[i],
-                        cl[i], cd[i], cdx[i]))
-                print "Warning: %s" % msg
-        else:
-            for dvar in self.dvars:
-                if isinstance(dvar, NomialArray):
-                    num = [result(x) for x in dvar]
-                else:
-                    num = result(dvar)
-                direct = None
-                if any(x < self.bounds[dvar][0] for x in hstack([num])):
-                    direct = "lower"
-                    bnd = self.bounds[dvar][0]
-                if any(x > self.bounds[dvar][1] for x in hstack([num])):
-                    direct = "upper"
-                    bnd = self.bounds[dvar][1]
+        for dvar in self.dvars:
+            if isinstance(dvar, NomialArray):
+                num = [result(x) for x in dvar]
+            else:
+                num = result(dvar)
+            direct = None
+            if any(x < self.bounds[dvar][0] for x in hstack([num])):
+                direct = "lower"
+                bnd = self.bounds[dvar][0]
+            if any(x > self.bounds[dvar][1] for x in hstack([num])):
+                direct = "upper"
+                bnd = self.bounds[dvar][1]
 
-                if direct:
-                    msg = ("Variable %.100s could cause inaccurate result"
-                           " because it exceeds" % dvar
-                           + " %s bound. Solution is %.4f but"
-                           " bound is %.4f" %
-                           (direct, amax([num]), bnd))
-                    print "Warning: " + msg
+            if direct:
+                msg = ("Variable %.100s could cause inaccurate result"
+                       " because it exceeds" % dvar
+                       + " %s bound. Solution is %.4f but"
+                       " bound is %.4f" %
+                       (direct, amax([num]), bnd))
+                print "Warning: " + msg
+
+class XfoilFit(FitCS):
+    """
+    Special fit constraint set that can post-solve compare result to XFOIL
+    INPUTS
+    ------
+    airfoil:                            airfoil of fitted data
+                                            str (e.g. "xxx.dat", "naca xxxx")
+
+    """
+    def __init__(self, fitdata, ivar=None, dvars=None, name="", err_margin=None,
+                 airfoil=False):
+
+        super(XfoilFit, self).__init__(fitdata, ivar=ivar, dvars=dvars,
+                                       name=name, err_margin=err_margin)
+
+        self.airfoil = airfoil
+
+    def process_result(self, result):
+        """
+        if data comes from Xfoil and airfoil is provided check against xfoil
+        """
+        super(XfoilFit, self).process_result(result)
+
+        if amax([abs(result["sensitivities"]["constants"][self.mfac])]) < 1e-5:
+            return None
+        if not self.airfoil:
+            return None
+
+        from .xfoilWrapper import xfoil_comparison
+        cl, re = 0.0, 0.0
+        for dvar in self.dvars:
+            if "Re" in str(dvar):
+                re = result(dvar)
+            if "C_L" in str(dvar):
+                cl = result(dvar)
+        cd = result(self.ivar)
+        if not hasattr(cl, "__len__") and hasattr(re, "__len__"):
+            cl = [cl]*len(re)
+        err, cdx = xfoil_comparison(self.airfoil, cl, re, cd)
+        ind = where(err > 0.05)[0]
+        for i in ind:
+            msg = ("Drag error for %s is %.2f. Re=%.1f; CL=%.4f;"
+                   " Xfoil cd=%.6f, GP sol cd=%.6f" %
+                   (", ".join(self.ivar.descr["models"]), err[i], re[i],
+                    cl[i], cd[i], cdx[i]))
+            print "Warning: %s" % msg
+
