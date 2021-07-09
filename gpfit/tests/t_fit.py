bd3b9ca18cfddc01a014d5a4b41d84788356c5dc
Philippe Kirschen <philippe@hyperloop-one.com>
2021-07-09 15:47:38 -0700

    Minor fixes
    * uniformly spaces (vs tabs)
    * need self.
    * assertRaises instead of assertEqual

--- gpfit/tests/t_fit.py
+++ gpfit/tests/t_fit.py
@@ -1,27 +1,28 @@
 #unit tests for gpfit.fit module"
 import unittest
+from numpy import logspace, log10, log, vstack
 from gpfit.fit import fit
 
 class t_fit(unittest.TestCase):
-	m = 501
-	u = logspace(0,log10(3),501)
-	w = (u**2 + 3)/(u+1)**2
-	x = log(u)
-	y = log(w)
-	K = 3
 
-	def test_rms_error(self):
-		cstrt, rms_error = fit(x, y, K, "SMA")
-		self.assertTrue(self.rms_error < 1e-4)
-		cstrt, rms_error = fit(x, y, K, "ISMA")
-		self.assertTrue(self.rms_error < 1e-5)
-		cstrt, rms_error = fit(x, y, K, "MA")
-		self.assertTrue(self.rms_error < 1e-2)
+    u = logspace(0, log10(3), 501)
+    w = (u**2 + 3)/(u+1)**2
+    x = log(u)
+    y = log(w)
+    K = 3
 
-	def test_incorrect_inputs(self):
-		err = fit(x, 0, K, "MA")
-		self.assertEqual(self.err, 'Dependent data should be a 1D numpy array')
-		
+    def test_rms_error(self):
+        cstrt, rms_error = fit(self.x, self.y, self.K, "SMA")
+        self.assertTrue(rms_error < 1e-4)
+        cstrt, rms_error = fit(self.x, self.y, self.K, "ISMA")
+        self.assertTrue(rms_error < 1e-5)
+        cstrt, rms_error = fit(self.x, self.y, self.K, "MA")
+        self.assertTrue(rms_error < 1e-2)
+
+    def test_incorrect_inputs(self):
+        with self.assertRaises(ValueError):
+            fit(self.x, vstack((self.y, self.y)), self.K, "MA")
+
 
 TESTS = [t_fit]
 
@@ -33,5 +34,3 @@ if __name__ == '__main__':
         SUITE.addTests(LOADER.loadTestsFromTestCase(t))
 
     unittest.TextTestRunner(verbosity=2).run(SUITE)
- 
-	
